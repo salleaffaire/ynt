e8009eb2282cc0548f94f869af0a131e0f0b80fb
DESKTOP-2UDOJVU\Luc <salleaffaire@gmail.com>
2021-09-09 04:35:06 -0400

    Adding lexer tests

--- lexer/lexer_test.go
+++ lexer/lexer_test.go
@@ -77,36 +77,92 @@ func TestNextToken(t *testing.T) {
 	}
 }
 
-func TestNextTokenNUmber(t *testing.T) {
-	input := "000"
+func TestNextTokenNumber(t *testing.T) {
 
 	tests := []struct {
-		expectedType    token.TokenType
-		expectedLiteral string
+		input    string
+		expected []struct {
+			Type    token.TokenType
+			Literal string
+		}
 	}{
-		{token.NUMBER, "0"},
-		{token.EOF, ""},
+		{"000", []struct {
+			Type    token.TokenType
+			Literal string
+		}{
+			{token.NUMBER, "0"},
+			{token.NUMBER, "0"},
+			{token.NUMBER, "0"},
+			{token.EOF, ""},
+		}},
+		{"{1,true}", []struct {
+			Type    token.TokenType
+			Literal string
+		}{
+			{token.LBRACE, "{"},
+			{token.NUMBER, "1"},
+			{token.COMMA, ","},
+			{token.TRUE, "true"},
+			{token.RBRACE, "}"},
+			{token.EOF, ""},
+		}},
+		{"0.48e-8", []struct {
+			Type    token.TokenType
+			Literal string
+		}{
+			{token.NUMBER, "0.48e-8"},
+			{token.EOF, ""},
+		}},
+		{"-0.48E8", []struct {
+			Type    token.TokenType
+			Literal string
+		}{
+			{token.NUMBER, "-0.48E8"},
+			{token.EOF, ""},
+		}},
+		{"-1000000000000000000000000000", []struct {
+			Type    token.TokenType
+			Literal string
+		}{
+			{token.NUMBER, "-1000000000000000000000000000"},
+			{token.EOF, ""},
+		}},
+		{"-1000000000000000000000000000", []struct {
+			Type    token.TokenType
+			Literal string
+		}{
+			{token.NUMBER, "-1000000000000000000000000000"},
+			{token.EOF, ""},
+		}},
+		{"\"abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!@#$%^&*)(\\\\\t\n\b\f\r0123456789\"", []struct {
+			Type    token.TokenType
+			Literal string
+		}{
+			{token.STRING, "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!@#$%^&*)(\\\\\t\n\b\f\r0123456789"},
+			{token.EOF, ""},
+		}},
 	}
 
-	l := New(input)
-
 	for i, tt := range tests {
-		// fmt.Println("Test : ", tt.expectedType)
-		tok := l.NextToken()
+		l := New(tt.input)
 
-		if tok.Type != tt.expectedType {
-			t.Fatalf("tests[%d] - tokentype wrong. expected=%q, got=%q",
-				i, tt.expectedType, tok.Type)
+		for j, e := range tt.expected {
+			tok := l.NextToken()
+
+			if tok.Type != e.Type {
+				t.Fatalf("tests[%d] - tokentype [%d] wrong. expected=%q, got=%q",
+					i, j, e.Type, tok.Type)
+			}
+
+			if tok.Literal != e.Literal {
+				t.Fatalf("tests[%d] - literal [%d] wrong. expected=%q, got=%q",
+					i, j, e.Literal, tok.Literal)
+			}
+
+			fmt.Println("Token Type   : ", tok.Type)
+			fmt.Println("Token Literal: ", tok.Literal)
+			fmt.Println()
 		}
-
-		if tok.Literal != tt.expectedLiteral {
-			t.Fatalf("tests[%d] - litearl wrong. expected=%q, got=%q",
-				i, tt.expectedLiteral, tok.Literal)
-		}
-
-		fmt.Println("Token Type   : ", tok.Type)
-		fmt.Println("Token Literal: ", tok.Literal)
-		fmt.Println()
 
 	}
 }
